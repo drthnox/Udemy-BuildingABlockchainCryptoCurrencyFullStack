3f6ac2707612e7006a717b48aa55ce6b605decf2
DavidG <dg_codemonkey@outlook.com>
2022-01-20 16:16:36 +0800

    Fixing/refactoring tests

--- pubsub.test.js
+++ pubsub.test.js
@@ -6,9 +6,10 @@ const redis = require('redis');
 
 describe('PubSub()', () => {
 
+  const blockchain = new Blockchain();
+
   describe('constructor()', () => {
     it('should create a new PubSub with default attributes', () => {
-      const blockchain = new Blockchain();
       const redisSpy = jest.spyOn(redis, 'createClient');
 
       const pubsub = new PubSub({ blockchain });
@@ -16,60 +17,42 @@ describe('PubSub()', () => {
       should.notEqual(pubsub.publisher, undefined);
       should.notEqual(pubsub.subscriber, undefined);
       expect(redisSpy).toBeCalled();
-    });
-
-    it('should store a copy of the blockchain passed to it', () => {
-      const blockchain = new Blockchain();
-
-      const pubsub = new PubSub({ blockchain });
-
       should.notEqual(pubsub.blockchain, undefined);
     });
   });
 
   describe('init()', () => {
-    const blockchain = new Blockchain();
     const pubsub = new PubSub({ blockchain });
     const subscriberSpy = jest.spyOn(pubsub.subscriber, 'subscribe');
     const onMessageSpy = jest.spyOn(pubsub.subscriber, 'on');
 
-    beforeEach(() => {
-      pubsub.init();
-    });
-
-    afterEach(() => {
-      // pubsub.reset();
-    });
-
     it('should initialise by subscribing to the CHANNELS', () => {
+      pubsub.init();
+
       expect(subscriberSpy).toHaveBeenCalledWith(CHANNELS.TEST);
       expect(subscriberSpy).toHaveBeenCalledWith(CHANNELS.BLOCKCHAIN);
-    });
-
-    it('should define on message callback', () => {
       expect(onMessageSpy).toBeCalled();
     });
   });
 
   describe('handleMessage()', () => {
 
-    it('should not attempt to replace the chain if a badly-formatted message is received on the BLOCKCHAIN channel', () => {
-      const blockchain = new Blockchain();
-      const pubsub = new PubSub({ blockchain });
-      const errSpy = jest.spyOn(console, 'error');
+    const pubsub = new PubSub({ blockchain });
+    const errSpy = jest.spyOn(console, 'error');
 
+    beforeEach(() => {
+      errSpy.mockClear();
+    });
+
+    it('should not attempt to replace the chain if a badly-formatted message is received on the BLOCKCHAIN channel', () => {
       pubsub.handleMessage({ channel: CHANNELS.BLOCKCHAIN, message: 'blah blah' });
 
       expect(errSpy).toHaveBeenCalledWith('Unexpected token b in JSON at position 0');
     });
 
     it('should attempt to replace the chain if a message is received on the BLOCKCHAIN channel', () => {
-      const blockchain = new Blockchain();
-      const pubsub = new PubSub({ blockchain });
       const blockchainSpy = jest.spyOn(blockchain, 'replaceChain');
       const parseSpy = jest.spyOn(JSON, 'parse');
-      const errSpy = jest.spyOn(console, 'error');
-      errSpy.mockClear();
 
       pubsub.handleMessage({ channel: CHANNELS.BLOCKCHAIN, message: JSON.stringify(blockchain.chain)});
 
@@ -80,11 +63,11 @@ describe('PubSub()', () => {
   });
 
   describe('subscribeToChannels()', () => {
-    const blockchain = new Blockchain();
-    const pubsub = new PubSub({ blockchain });
-    const subscriberSpy = jest.spyOn(pubsub.subscriber, 'subscribe');
 
     it('should subscribe to channels', () => {
+      const pubsub = new PubSub({ blockchain });
+      const subscriberSpy = jest.spyOn(pubsub.subscriber, 'subscribe');
+
       pubsub.subscribeToChannels();
 
       Object.values(CHANNELS).forEach(channel => {
@@ -94,12 +77,12 @@ describe('PubSub()', () => {
   });
 
   describe('Publishing to channels', () => {
-    const blockchain = new Blockchain();
     const pubsub = new PubSub({ blockchain });
     const publisherSpy = jest.spyOn(pubsub.publisher, 'publish');
 
     beforeEach(() => {
       publisherSpy.mockImplementation(({ channel, message }) => { });
+      publisherSpy.mockClear();
     });
 
     describe('publish()', () => {
