4464174fba0898bc725d544440428f377bb75038
DavidG <dg_codemonkey@outlook.com>
2022-01-24 16:06:23 +0800

    Added random ports for peers

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
+const request = require('request');
 
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 
@@ -16,14 +21,26 @@ app.get('/api/blocks', (req, res) => {
 app.post('/api/mine', (req, res) => {
   const { data } = req.body;
   blockchain.addBlock({ data });
+  pubsub.broadcastChain();
   res.redirect('/api/blocks');
 });
 
+const syncChains = () => {
+  request({ur: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error,response,body) => {
+    if(!error && response.statusCode == 200) {
+      const rootChain = JSON.parse(body);
+      console.log('Replace chain on a sync with', rootChain);
+      blockchain.replaceChain(rootChain);
+    }
+  });
+};
 
-const PORT = 3000;
+let PEER_PORT;
+if(process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
   console.log(`Listening on localhost:${PORT}`);
 });
-
-
-setTimeout(() => pubsub.broadcastChain(), 1000);
