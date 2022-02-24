a9f293f734708b03a42f507a056834e28edfa56f
DavidG <dg_codemonkey@outlook.com>
2022-02-24 16:55:22 +0800

    Mocked console to minimise output.

--- util/crypto-hash.test.js
+++ util/crypto-hash.test.js
@@ -2,6 +2,17 @@ const cryptoHash = require('./crypto-hash');
 
 describe('cryptoHash()', () => {
 
+  beforeEach(() => {
+    mockConsole();
+  });
+
+  function mockConsole() {
+    errMock = jest.fn();
+    logMock = jest.fn();
+    global.console.error = errMock;
+    global.console.log = logMock;
+  }
+
   it('generates a SHA256 hashed output', () => {
     expect(cryptoHash('foo')).toEqual('2C26B46B68FFC68FF99B453C1D30413413422D706483BFA0F98A5E886266E7AE'.toLowerCase());
     // expect(cryptoHash('foo')).toEqual('0010110000100110101101000110101101101000111111111100011010001111111110011001101101000101001111000001110100110000010000010011010000010011010000100010110101110000011001001000001110111111101000001111100110001010010111101000100001100010011001101110011110101110');
