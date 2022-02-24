b37faba11b87a8873a908a25e874e474d247cde4
DavidG <dg_codemonkey@outlook.com>
2022-02-24 18:08:52 +0800

    Added signing and verifying signatures

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,6 @@
 const { STARTING_BALANCE } = require('../config');
 const Wallet = require('./index');
+const { verifySignature } = require('../util');
 
 describe('Wallet', () => {
 
@@ -16,7 +17,38 @@ describe('Wallet', () => {
 
   it('has a `publicKey`', () => {
     expect(wallet).toHaveProperty('publicKey');
+    // console.log(wallet.publicKey);
   });
 
+  describe('signing data', () => {
+    const data = 'foobar';
+
+    it('verifies a signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: wallet.sign(data)
+        })
+      ).toBe(true);
+    });
+
+    it('does not verify an invalid signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: new Wallet().sign(data)
+        })
+      ).toBe(false);
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: wallet.sign(data + 'X')
+        })
+      ).toBe(false);
+    });
+  });
 
 });
