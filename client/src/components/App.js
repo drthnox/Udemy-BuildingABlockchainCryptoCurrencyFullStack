13c4c31749d000ae3d8a348910f167acf5a9f758
DavidG <dg_codemonkey@outlook.com>
2022-06-22 11:55:47 +0800

    Temp checkin while no console errors

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,9 +8,10 @@ class App extends Component {
   render() {
     return (
       <div>
+        hello
         <div>Wallet Info:</div>
-        <div>Address: {address}</div>
-        <div>Balance: {balance}</div>
+        {/* <div>Address: {address}</div> */}
+        {/* <div>Balance: {balance}</div> */}
       </div>
     );
   }
